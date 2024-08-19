ff9564489da03557b915fad35ee8e5e01a1acc5d
Mohd Sameer <mesh789736@gmail.com>
2024-08-19 16:25:16 +0530

    added two more api routes for category product and simple product

---
+++ src/app/api/product/singleProduct/route.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient()
+
+export async function POST (req: NextRequest) {
+    try {
+        const body = await req.json()
+
+        const {productId} = body
+
+        const response = await prisma.product.findUnique({
+            where: {
+                productId
+            }
+        })
+
+        console.log(response)
+
+        return NextResponse.json({statusCode: 200, response, status: true})
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({statusCode: 404, message: 'Unable to find the prodcut', status: false})
+    }
+}
